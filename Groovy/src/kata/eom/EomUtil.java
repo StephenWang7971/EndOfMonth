12fde0876489329afc21f922c3f7288008c0b4ff
StephenWang <stephen.wang.7971@gmail.com>
2014-05-04 08:28:32 +0800

    Add Groovy Skeleton

---
+++ Groovy/src/kata/eom/EomUtil.java
@@ -0,0 +1,10 @@
+package kata.eom;
+
+import java.util.Date;
+
+class EomUtil {
+
+    static Date getEndOfMonth(Date given) {
+        return null;
+    }
+}
---
+++ Groovy/test/kata/eom/TestEom.java
@@ -0,0 +1,4 @@
+package kata.eom;
+
+class TestEom {
+}
