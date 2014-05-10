c28ad5153b9204e9032bd591d145c01219c2e8fd
StephenWang <stephen.wang.7971@gmail.com>
2014-05-10 08:23:32 +0800

    implement first version
    It is still RED.

--- Java/src/main/kata/eom/EomUtil.java
+++ Java/src/main/kata/eom/EomUtil.java
@@ -1,10 +1,20 @@
 package kata.eom;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class EomUtil {
 	
 	public static Date getEndDateOfMonth(Date given) {
-		return null;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(given);
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+
+        calendar.set(Calendar.DATE, 31);   //This causes error in other montht.
+
+		return calendar.getTime();
 	}
 }
