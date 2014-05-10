545d80523c86a95ecd14c8982da1bab62a0a66af
StephenWang <stephen.wang.7971@gmail.com>
2014-05-10 08:17:33 +0800

    Initialize tests.
    It is RED, because no working codes at the moment.

--- Java/src/test/kata/eom/TestEomUtil.java
+++ Java/src/test/kata/eom/TestEomUtil.java
@@ -1,4 +1,66 @@
 package kata.eom;
 
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestEomUtil {
+
+    @Test
+    public void test135781012 () {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2013);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DATE, 12);
+        Date eom = EomUtil.getEndDateOfMonth(calendar.getTime());
+        Calendar target = Calendar.getInstance();
+        target.setTime(eom);
+        assertEquals(2013, target.get(Calendar.YEAR));
+        assertEquals(0, target.get(Calendar.MONTH));
+        assertEquals(31, target.get(Calendar.DATE));
+    }
+
+    public void test46911() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2013);
+        calendar.set(Calendar.MONTH, 3);
+        calendar.set(Calendar.DATE, 15);
+        Date eom = EomUtil.getEndDateOfMonth(calendar.getTime());
+        Calendar target = Calendar.getInstance();
+        target.setTime(eom);
+        assertEquals(2013, target.get(Calendar.YEAR));
+        assertEquals(3, target.get(Calendar.MONTH));
+        assertEquals(30, target.get(Calendar.DATE));
+    }
+
+    public void test2() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2013);
+        calendar.set(Calendar.MONTH, 1);
+        calendar.set(Calendar.DATE, 15);
+        Date eom = EomUtil.getEndDateOfMonth(calendar.getTime());
+        Calendar target = Calendar.getInstance();
+        target.setTime(eom);
+        assertEquals(2013, target.get(Calendar.YEAR));
+        assertEquals(1, target.get(Calendar.MONTH));
+        assertEquals(28, target.get(Calendar.DATE));
+    }
+
+    public void testLeap2() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2013);
+        calendar.set(Calendar.MONTH, 1);
+        calendar.set(Calendar.DATE, 15);
+        Date eom = EomUtil.getEndDateOfMonth(calendar.getTime());
+        Calendar target = Calendar.getInstance();
+        target.setTime(eom);
+        assertEquals(2012, target.get(Calendar.YEAR));
+        assertEquals(1, target.get(Calendar.MONTH));
+        assertEquals(29, target.get(Calendar.DATE));
+    }
+
 }
